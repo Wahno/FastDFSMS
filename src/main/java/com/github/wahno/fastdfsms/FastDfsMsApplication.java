5246b680260023d8664b9708513dd5461d6b4243
WANGHAO <1727386617@qq.com>
2019-11-26 15:47:01 +0800

    Initial commit

---
+++ src/main/java/com/github/wahno/fastdfsms/FastDfsMsApplication.java
@@ -0,0 +1,18 @@
+package com.github.wahno.fastdfsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author WANGHAO 2019-11-26
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class FastDfsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastDfsMsApplication.class, args);
+    }
+
+}
